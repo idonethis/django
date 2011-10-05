253996035b09ca431d55ac5727fc970b3eac8c75
Rodrigo Guzman <rodguze@gmail.com>
2011-10-05 13:52:50 -0700

    make createsuperuser use accounts.User

--- django/contrib/auth/management/commands/createsuperuser.py
+++ django/contrib/auth/management/commands/createsuperuser.py
@@ -6,7 +6,8 @@ import getpass
 import re
 import sys
 from optparse import make_option
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from accounts.models import User
 from django.core import exceptions
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import ugettext as _
